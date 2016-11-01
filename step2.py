41f5aeb79d955d44d50bc254d63d0ee5a00f5bf2
Ekua Awotwi <eawotwi@stanford.edu>
2016-10-31 20:54:13 -0700

    new string reversal method

--- step2.py
+++ step2.py
@@ -14,7 +14,8 @@ r = requests.post(url, json=data)
 
 # reverse string #
 original_string = r.text
-reversed_string = reverse(original_string)
+reversed_string = original_string[::-1] #reverse string using extended slice operator
+
 
 # HTTP POST request #
 url = 'http://challenge.code2040.org/api/reverse/validate'
