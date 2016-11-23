3691dae7988bb7fd1510b038457c2e886a461659
Ekua Awotwi <eawotwi@stanford.edu>
2016-11-23 03:29:54 -0500

    added error and exception handling

--- post_request.py
+++ post_request.py
@@ -1,3 +1,4 @@
+import sys
 import requests
 from credentials import base_url, registration_details
 
@@ -9,13 +10,25 @@ token = registration_details['token']
 data = {'token' : token}
 
 def retrieval(specific_endpoint):
-	response = requests.post(base_url + specific_endpoint, json = data)
-	return response
+	try:
+		response = requests.post(base_url + specific_endpoint, json = data)
+		response.raise_for_status()
+		return response
+
+	except requests.exceptions.RequestException as e:
+		print e
+		sys.exit('Incomplete step: unable to retrieve information')
+		
 
 def validation(specific_endpoint, return_value):
 	data.update(return_value)
 	if not (specific_endpoint == 'register'): #excludes step 1
 		specific_endpoint += '/validate'
 
-	response = requests.post(base_url + specific_endpoint, json = data)
-	print response.text
+	try: 
+		response = requests.post(base_url + specific_endpoint, json = data)
+		response.raise_for_status()
+
+	except requests.exceptions.RequestException as e:
+		print e
+		sys.exit('Incomplete step: unable to validate information')
